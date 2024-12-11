503712d28c69d7bd1ec6c1b5e0efd7e6ecb4111e
abbey-glitch <abiodunonaolapi@gmail.com>
2024-12-11 10:18:54 +0100

    Update certmanager.sh

--- certmanager.sh
+++ certmanager.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-$DOMAIN_NAME = "testblog.testdomain.com"
-CERTBOT_PATH=$(command -v certbot || echo "/snap/bin/certbot")
 
-if [ ! -x "$CERTBOT_PATH" ]; then
-  echo "Certbot is not installed or not in PATH. Please install Certbot and try again."
-  exit 1
-fi
-
-# Use Certbot
-$CERTBOT_PATH certonly --standalone -d "$DOMAIN_NAME"
