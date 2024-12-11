3aaaa42de779e4c73b399363d543c7a5ff563d33
abbey-glitch <abiodunonaolapi@gmail.com>
2024-12-11 10:18:25 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-$DOMAIN_NAME = "testblog.testdomain.com"
-# Example usage of the DOMAIN_NAME variable
-if [ -z "$DOMAIN_NAME" ]; then
-  echo "DOMAIN_NAME is not set!"
-  exit 1
-fi
 
-echo "Initializing setup for domain: $DOMAIN_NAME"
-# Additional setup commands
